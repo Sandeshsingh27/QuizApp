57cfa2457272064be91ac4de1213db3cc521bbcd
Sandesh Singh <sandeshsingh295@gmail.com>
2024-06-19 13:59:58 +0530

    added Exception handling and returning HTTP status code with every operations

--- src/main/java/com/sandesh/QuizApp/controller/QuestionController.java
+++ src/main/java/com/sandesh/QuizApp/controller/QuestionController.java
@@ -1,9 +1,9 @@
 package com.sandesh.QuizApp.controller;
 
-
 import com.sandesh.QuizApp.model.Question;
 import com.sandesh.QuizApp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,32 +16,32 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("/allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 
     @GetMapping("/category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable("category") String category){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable("category") String category) {
         return questionService.getQuestionsByCategory(category);
     }
 
     @GetMapping("/level/{level}")
-    public List<Question> getQuestionsByDifficultyLevel(@PathVariable("level") String level){
+    public ResponseEntity<List<Question>> getQuestionsByDifficultyLevel(@PathVariable("level") String level) {
         return questionService.getQuestionsByDifficultyLevel(level);
     }
 
     @PostMapping("/add")
-    public Question addQuestion(@RequestBody Question question){
+    public ResponseEntity<Question> addQuestion(@RequestBody Question question) {
         return questionService.addQuestion(question);
     }
 
     @PutMapping("/update/{id}")
-    public Question updateQuestion(@PathVariable("id") int id, @RequestBody Question question) {
+    public ResponseEntity<Question> updateQuestion(@PathVariable("id") int id, @RequestBody Question question) {
         return questionService.updateQuestion(id, question);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteQuestion(@PathVariable("id") int id) {
-        questionService.deleteQuestion(id);
+    public ResponseEntity<Void> deleteQuestion(@PathVariable("id") int id) {
+        return questionService.deleteQuestion(id);
     }
 }
--- src/main/java/com/sandesh/QuizApp/service/QuestionService.java
+++ src/main/java/com/sandesh/QuizApp/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.sandesh.QuizApp.service;
 import com.sandesh.QuizApp.dao.QuestionDao;
 import com.sandesh.QuizApp.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,41 +17,81 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
-    }
-
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionDao.findByCategoryIgnoreCase(category);
-    }
-
-    public List<Question> getQuestionsByDifficultyLevel(String level) {
-        return questionDao.findByDifficultyLevelIgnoreCase(level);
-    }
-
-    public Question addQuestion(Question question) {
-        return questionDao.save(question);
-    }
-
-    public Question updateQuestion(int id, Question question) {
-        Optional<Question> optionalQuestion = questionDao.findById(id);
-        if (optionalQuestion.isPresent()) {
-            Question existingQuestion = optionalQuestion.get();
-            existingQuestion.setCategory(question.getCategory());
-            existingQuestion.setDifficultyLevel(question.getDifficultyLevel());
-            existingQuestion.setQuestionTitle(question.getQuestionTitle());
-            existingQuestion.setOption1(question.getOption1());
-            existingQuestion.setOption2(question.getOption2());
-            existingQuestion.setOption3(question.getOption3());
-            existingQuestion.setOption4(question.getOption4());
-            existingQuestion.setRightAnswer(question.getRightAnswer());
-            return questionDao.save(existingQuestion);
-        } else {
-            return null; // or throw an exception if preferred
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            List<Question> questions = questionDao.findAll();
+            return new ResponseEntity<>(questions, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public void deleteQuestion(int id) {
-        questionDao.deleteById(id);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            List<Question> questions = questionDao.findByCategoryIgnoreCase(category);
+            return new ResponseEntity<>(questions, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByDifficultyLevel(String level) {
+        try {
+            List<Question> questions = questionDao.findByDifficultyLevelIgnoreCase(level);
+            return new ResponseEntity<>(questions, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<Question> addQuestion(Question question) {
+        try {
+            Question savedQuestion = questionDao.save(question);
+            return new ResponseEntity<>(savedQuestion, HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<Question> updateQuestion(int id, Question question) {
+        try {
+            Optional<Question> optionalQuestion = questionDao.findById(id);
+            if (optionalQuestion.isPresent()) {
+                Question existingQuestion = optionalQuestion.get();
+                existingQuestion.setCategory(question.getCategory());
+                existingQuestion.setDifficultyLevel(question.getDifficultyLevel());
+                existingQuestion.setQuestionTitle(question.getQuestionTitle());
+                existingQuestion.setOption1(question.getOption1());
+                existingQuestion.setOption2(question.getOption2());
+                existingQuestion.setOption3(question.getOption3());
+                existingQuestion.setOption4(question.getOption4());
+                existingQuestion.setRightAnswer(question.getRightAnswer());
+                Question updatedQuestion = questionDao.save(existingQuestion);
+                return new ResponseEntity<>(updatedQuestion, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<Void> deleteQuestion(int id) {
+        try {
+            if (questionDao.existsById(id)) {
+                questionDao.deleteById(id);
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
